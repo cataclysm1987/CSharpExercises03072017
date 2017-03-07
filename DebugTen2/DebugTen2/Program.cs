c087bcf9674bcfa198a1b3b1ef0493229ff7e292
cataclysm1987 <conklin.eric@gmail.com>
2017-03-07 13:33:22 -0500

    add files for debug

---
+++ DebugTen2/DebugTen2/Program.cs
@@ -0,0 +1,92 @@
+﻿// Street is an abstract class
+// OneWayStreet and TwoWayStreet derive from Street
+// On a OneWayStreet, it is illegal to make a U turn
+// On a TwoWayStreet, a U Turn reverses the travelling direction
+
+// Main program creates two Street child objects - one OneWay and one TwoWay
+// and demonstrates what happens when you make a U Turn
+// on a OneWayStreet and a TwoWayStreet
+
+using System;
+using static System.Console;
+class DebugTen02
+{
+    static void Main()
+    {
+        OneWayStreet oak = new OneWayStreet("Oak Avenue", "east");
+        TwoWayStreet elm = new TwoWayStreet("Elm Street", "south");
+        WriteLine("On " + oak.Name + " " + oak.MakeUTurn());
+        WriteLine("On " + oak.Name + " " + oak.MakeUTurn());
+        WriteLine("On " + elm.Name + " " + elm.MakeUTurn());
+        WriteLine("On " + elm.Name + " " + elm.MakeUTurn());
+    }
+}
+abstract class Street
+{
+    protected string name;
+    public string direction;
+    public Street(string n, string travelDirection)
+    {
+        name = n;
+        direction = travelDirection;
+    }
+
+    protected Street()
+    {
+        name = "Main";
+        direction = "south";
+    }
+
+    public string Name
+    {
+        get
+        {
+            return name;
+        }
+    }
+    public abstract string MakeUTurn();
+}
+class OneWayStreet : Street
+{
+    public OneWayStreet(string n, string travelDirection)
+    {
+        name = n;
+        direction = travelDirection;
+    }
+
+    public override string MakeUTurn()
+    {
+        string temp = "U Turn is illegal! Was going and still going " + direction;
+        return temp;
+    }
+}
+class TwoWayStreet : Street
+{
+
+    public TwoWayStreet(string name, string direction) : base(name, direction)
+    {
+    }
+
+    public override string MakeUTurn()
+    {
+        string wasGoing = direction;
+        string[] directions = {"north", "south", "east", "west"};
+        string[] oppDirections = {"south", "north", "west", "east"};
+        for (int x = 0; x < directions.Length; ++x)
+        {
+            if (direction.Equals(directions[x]))
+            {
+                direction = oppDirections[x];
+                x = directions.Length;
+            }
+        }
+
+
+        string temp = "U Turn successful. Was going " + wasGoing +
+                      ". Now going " + direction;
+        return temp;
+    }
+
+    
+}
+
---
+++ DebugTen4/DebugTen4/Program.cs
@@ -0,0 +1,68 @@
+﻿// class HotelRoom has child classes SingleRoom which costs less,
+// and Suite which costs more
+using static System.Console;
+class DebugTen04
+{
+    static void Main()
+    {
+        HotelRoom aRoom = new HotelRoom(234);
+        SingleRoom aSingle = new SingleRoom(135);
+        Suite aSuite = new Suite(453);
+        WriteLine(aRoom.ToString());
+        WriteLine(aSingle.ToString());
+        WriteLine(aSuite.ToString());
+    }
+}
+class HotelRoom
+{
+    public const double PREMIUM = 30.00;
+    public const double STD_RATE = 89.99;
+    protected int roomNumber;
+    protected double rate;
+    public HotelRoom(int room)
+    {
+        roomNumber = room;
+        rate = STD_RATE;
+    }
+
+    protected HotelRoom()
+    {
+        roomNumber = 1;
+        rate = STD_RATE;
+    }
+
+    public int RoomNumber => roomNumber;
+
+    private int roomNum { get; set; }
+
+    
+    public override string ToString()
+    {
+        string temp = GetType() + " Room " + RoomNumber + " Rate: " +
+          Rate.ToString("C");
+        return temp;
+    }
+
+    public double Rate
+    {
+        get { return rate; } 
+        set { rate = value; }
+    }
+}
+class SingleRoom : HotelRoom
+{
+   public SingleRoom(int room) : base (room)
+   {
+    rate -= PREMIUM;
+}   
+}
+class Suite : HotelRoom
+{
+   public Suite(int room)
+   {
+       rate += PREMIUM;
+       roomNumber = room;
+   }
+
+}
+
